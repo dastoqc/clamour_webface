c24025f1f2eaf2d4a5110203d09f84e4e48fd2d5
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2019-07-17 15:51:43 -0400

    First implementation of simple tcp server.

--- clamour_server/controllers/management/manage_tcp.js
+++ clamour_server/controllers/management/manage_tcp.js
@@ -1,5 +1,34 @@
-var Client = require('ssh2').Client;
+var net = require("net");
+var colors = require("colors");
+
+// TCP server creation
+var server = net.createServer();
 
 exports.listen_to_data_stream = function (req, res, next) {
-    console.log("You've got pretty good at making routes ;)")
+    server.listen(3001, '127.0.0.1', function () {
+        console.log(`TCP server listening to ${server.address().address}:${server.address().port}`.blue);
+    }).on("connection", handleTcpConnection);
+}
+
+function handleTcpConnection(socket) {
+    var remoteAddress = socket.remoteAddress + ':' + socket.remotePort;
+    console.log("new client connection from %s".green, remoteAddress);
+
+    // socket.on  ("ready",    onReady);  
+    // socket.on  ("timeout",  onTimeOut);
+    socket.on  ("data",     onEventData);
+    socket.once("close",    onEventClose);
+    socket.on  ("error",    onEventError);
+
+    function onEventData(data) {
+        console.log(`TCP connection data from ${remoteAddress}: ${data.toString("ascii")}`.cyan);
+    }
+
+    function onEventClose() {
+        console.log(`TCP connection from ${remoteAddress} closed`.yellow);
+    }
+
+    function onEventError(err) {
+        console.log(`TCP connection ${remoteAddress} error: ${err.message}`.red);
+    }
 }
