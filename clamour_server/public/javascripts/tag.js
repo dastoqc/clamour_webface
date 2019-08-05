0faab22198a2cf0f5e2b82e15941180ff8660811
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2019-08-05 16:33:13 -0400

    Testing a small responsive button

--- clamour_server/public/javascripts/tag.js
+++ clamour_server/public/javascripts/tag.js
@@ -1,12 +1,15 @@
 var network = new Vue({
     el:'#vue-network',
     data: {
-        id: ["123", "456", "678", "910"]
+        scan_done: false,
+        ip_addresses: ["123", "456", "678", "910"],
+        selected: ""
     },
     
     methods: {
         scan_network: function() {
-            this.data.id = ["abc", "def", "ghi", "jkl"];
+            this.ip_addresses = ["abc", "def", "ghi", "jkl"];
+            this.scan_done = true;
         }
     }
 })
