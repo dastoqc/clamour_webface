3494f42fcc5de457a7d69079b4d83bc66459ab45
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2019-08-14 15:17:18 -0400

    Addition of a console display after the network scan

--- clamour_server/controllers/management/network/manage_network.js
+++ clamour_server/controllers/management/network/manage_network.js
@@ -13,10 +13,11 @@ module.exports.scan_for_tag_ip_address = async function(req, res, next) {
 
     quickscan.on('complete', function (data) {
         var ip_addresses = ip_handler.get_ip_addresses_from_scan(data);
-        var tag_ip_addresses = ip_handler.get_tag_ip_addresses_from_list(ip_addresses);
+        var tag_ip_addresses = ip_handler.filter_ip_addresses_from_list(ip_addresses);
+        console.log(`List ip addresses detected on the network : ${tag_ip_addresses}`);
     });
     
     quickscan.on('error', function(err){
-        console.log(`An error occured : \n${err}`);
+        console.log(`An error occured while trying to scan the network :\n${err}`);
     });
 };
