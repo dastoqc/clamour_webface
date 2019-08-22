cfcb7c411a423d15e5ca0905cc4c47fcb2e91f6e
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2019-08-22 18:29:33 -0400

    First working interface capable of communicating with the backend

--- clamour_server/public/javascripts/management.js
+++ clamour_server/public/javascripts/management.js
@@ -1,39 +1,44 @@
 
 var network = new Vue({
-    el:'#vue-network',
+    el: '#vue-network',
     data: {
-        ip_addresses: [],
-        device_list: [{
-            id: Number,
-            ip_address: String,
-            running_status: String,
-            network_status: String
-        }],
-        selected: "Select network",
-        variable: {id: 1234, ip_address: "123.456.789.101", running_status: "OFF", network_status: "OUT"}
+        detected_device_list: [],
+        known_device_list: [],
+        selected: "192.168.4.200",
+        variable: { id: 1234, ip_address: "123.456.789.101", running_status: "OFF", network_status: "OUT" }
     },
-    
+
     methods: {
-        scan_network: function() {
-            axios.get("management/network")
-                .then((res) => {
-                    this.ip_addresses=res.data;
-                    console.log(res);
-                })
-                .catch((err) => {
-                    alert('Error during http call :', err)
-                    console.warn('Error during http call :', err);
-                })
-                .finally(() => {
-                });
+        scan_network: async function () {
+            try {
+                var response = await axios.get("scan_network");
+                this.detected_device_list = response.data;
+            } catch (err) {
+                alert(`An error occured while trying to scan the network\n`, err);
+                console.warn(`Error during http call :\n`, err);
+            }
         },
 
-        start_devices: function() {
-
+        start_devices: async function () {
+            try {
+                var response = await axios.get(`start_script/ip_address/${this.selected}/mode/visit`)
+                console.log(response);
+                //this.detected_device_list = response.data;
+                
+            } catch (err) {
+                alert(`An error occured while trying to start the localization on tag\n`, err);
+                console.log(`Error during http call :\n`, err);
+            }
         },
 
-        stop_download_devices: function() {
-            
+        stop_download_devices: async function () {
+            try {
+                var response = await axios.get(`stop_download/ip_address/${this.selected}`)
+                console.log(response);    
+            } catch (err) {
+                alert(`An error occured while trying to stop the localization on tag ${this.selected}\n`, err);
+                console.log(`Error during http call :\n`, err);
+            }
         }
     }
 })
