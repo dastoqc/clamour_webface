464b0d9b3ffcbc8b64102a2ce758f7ded11d0957
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2019-08-23 17:31:13 -0400

    Minimal changes to render a .vue component on the frontend

---
+++ clamour_server/public/components/management.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+    div
+      p#test_text This is just a test text field
+</template>
+
+<script>
+// Imports
+import Tag from "../components/tag_summary.vue";
+
+export default {
+  components: {
+    "tag_summary": Tag
+  },
+  data() {
+    return {
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML Wizardry", show: false },
+      ],
+      title: "Vue Wizards"
+    };
+  },
+  methods: {
+    updateTitle: function(updatedTitle) {
+      this.title = updatedTitle;
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
---
+++ clamour_server/public/components/tag_summary.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    div
+      p {{ title }}
+      | random text 
+</template>
+
+<script>
+// Imports
+export default {
+  data() {
+    return {
+      title: "Vue Wizards"
+    };
+  },
+  methods: {
+    updateTitle: function(updatedTitle) {
+      this.title = updatedTitle;
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
